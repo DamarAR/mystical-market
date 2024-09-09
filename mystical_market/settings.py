98a386cb8bdf31485fe5efed0091fcb906596ad9
Damar Aryaputra Rahman <156920499+DamarAR@users.noreply.github.com>
2024-09-09 20:30:46 +0700

    Update settings.py

--- mystical_market/settings.py
+++ mystical_market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6q@4xgix@rk&b6=xjkd65i4bslq6t98pj2*m)i_i8i9%scm#qu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'damar-aryaputra-mysticalmarket.pbp.cs.ui.ac.id']
 
 
 # Application definition
